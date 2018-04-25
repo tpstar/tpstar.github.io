46c2f2f1781d92a8c6cd39a3ad8572810ac5f96f
tpstar <hanlee12@gmail.com>
2018-04-24 23:00:00 -0500

    built sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -58,5 +58,15 @@ $(document).ready(function() {
         }
       });
 
+      /* For the sticky navigation */
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
 
 })
