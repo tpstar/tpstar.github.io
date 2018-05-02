797e1d31216259390428262f3dfe367a0a6436ff
tpstar <hanlee12@gmail.com>
2018-05-02 15:53:57 -0500

    implement grid masonry to blog section

--- resources/js/script.js
+++ resources/js/script.js
@@ -69,4 +69,35 @@ $(document).ready(function() {
       offset: '60px;'
     });
 
+    /* For Grid Masonry (Blog) */
+
+    function resizeGridItem(item){
+       grid = document.getElementsByClassName("grid")[0];
+       rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+       rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+       rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+       item.style.gridRowEnd = "span "+rowSpan;
+    }
+
+    function resizeAllGridItems(){
+       allItems = document.getElementsByClassName("item");
+       for(x=0;x<allItems.length;x++){
+          resizeGridItem(allItems[x]);
+       }
+    }
+
+    window.onload = resizeAllGridItems();
+
+    window.addEventListener("resize", resizeAllGridItems);
+
+    // allItems = document.getElementsByClassName("item");
+    // for(x=0;x<allItems.length;x++){
+    //    imagesLoaded( allItems[x], resizeInstance);
+    // }
+    //
+    // function resizeInstance(instance){
+    //    item = instance.elements[0];
+    //    resizeGridItem(item);
+    // }
+
 });
